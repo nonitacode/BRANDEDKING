27064961b47d0fa5b2ac8452292e7ffa831224a1
NOBITA <nobitaanetwork@gmail.com>
2025-09-11 22:34:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,21 +83,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/f5874a49d5acb00a19a7a.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/59au6c.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/4a810f39923161933f6e0.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/59au6c.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/59au6c.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/59au6c.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/59au6c.jpg"
 
 
 def time_to_seconds(time):
